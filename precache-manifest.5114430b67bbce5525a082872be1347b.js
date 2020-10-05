e5c198c17927283e18cc8370a9a0bb15cd5993b2
karthik-v-dev <karthikvoorugonda@gmail.com>
2020-10-06 03:01:29 +0530

    Updates

---
+++ precache-manifest.5114430b67bbce5525a082872be1347b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9242d9fb1939c87382f12a2e217a7c42",
+    "url": "/moboibookings/index.html"
+  },
+  {
+    "revision": "f9f74c4fea31638520e6",
+    "url": "/moboibookings/static/css/2.828be9a0.chunk.css"
+  },
+  {
+    "revision": "bb574681fe3994a5fc63",
+    "url": "/moboibookings/static/css/main.e23f46bd.chunk.css"
+  },
+  {
+    "revision": "f9f74c4fea31638520e6",
+    "url": "/moboibookings/static/js/2.a152b979.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/moboibookings/static/js/2.a152b979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb574681fe3994a5fc63",
+    "url": "/moboibookings/static/js/main.688105a7.chunk.js"
+  },
+  {
+    "revision": "83c806bff4ca2b826b87",
+    "url": "/moboibookings/static/js/runtime-main.979c6024.js"
+  },
+  {
+    "revision": "79a8ded71b8f676e52916ab1aed17cbd",
+    "url": "/moboibookings/static/media/Ibapp.79a8ded7.png"
+  },
+  {
+    "revision": "19c7609ec2a7cd82fb1202c87b9f24d0",
+    "url": "/moboibookings/static/media/img1.19c7609e.jpg"
+  },
+  {
+    "revision": "587c16feff36053675ef7c1a49e4afed",
+    "url": "/moboibookings/static/media/img10.587c16fe.jpg"
+  },
+  {
+    "revision": "456d23be1551fd5a7f6101a1ae3bba6c",
+    "url": "/moboibookings/static/media/img3.456d23be.jpg"
+  },
+  {
+    "revision": "17ac45c4395b6f4d136e912408298fed",
+    "url": "/moboibookings/static/media/img4.17ac45c4.jpg"
+  },
+  {
+    "revision": "322cba6907acbf32370a753ac8c3fe1f",
+    "url": "/moboibookings/static/media/img5.322cba69.jpg"
+  },
+  {
+    "revision": "3767d803baba697b3a24cbe5215e349a",
+    "url": "/moboibookings/static/media/img6.3767d803.jpg"
+  },
+  {
+    "revision": "d9bdea92848f5fba6a50a0fe3051a2ac",
+    "url": "/moboibookings/static/media/websiteimg11.d9bdea92.png"
+  }
+]);
